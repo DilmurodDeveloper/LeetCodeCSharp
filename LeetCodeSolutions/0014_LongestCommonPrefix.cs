253cb435275f9594005c65b0e769a716f01f63d3
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-20 12:02:55 +0500

    Implement Longest Common Prefix (LeetCode #14)

---
+++ LeetCodeSolutions/0014_LongestCommonPrefix.cs
@@ -0,0 +1,26 @@
+﻿//- - - - - - - - - - - - - - - - - - -
+// Longest Common Prefix - LeetCode #14
+//- - - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+
+            Array.Sort(strs);
+            string first = strs[0];
+            string last = strs[^1];
+            int i = 0;
+
+            while (i < first.Length && i < last.Length && first[i] == last[i])
+            {
+                i++;
+            }
+
+            return first.Substring(0, i);
+        }
+    }
+}
