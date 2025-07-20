30f9bdb7ec296b331969f2f491ad3af05018ef2c
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-20 12:09:42 +0500

    Implement Valid Parentheses (LeetCode #20)

---
+++ LeetCodeSolutions/0020_ValidParentheses.cs
@@ -0,0 +1,39 @@
+﻿//- - - - - - - - - - - - - - - - -
+// Valid Parentheses - LeetCode #20
+//- - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public bool IsValid(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char currentChar in input)
+            {
+                if (currentChar == '(' || currentChar == '{' || currentChar == '[')
+                {
+                    stack.Push(currentChar);
+                }
+                else
+                {
+                    if (stack.Count == 0 || !IsMatchingPair(stack.Peek(), currentChar))
+                    {
+                        return false;
+                    }
+                    stack.Pop();
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        private bool IsMatchingPair(char opening, char closing)
+        {
+            return (opening == '(' && closing == ')') ||
+                   (opening == '{' && closing == '}') ||
+                   (opening == '[' && closing == ']');
+        }
+    }
+}
