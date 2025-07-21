1d56219db60cd80b595eb7716fc10baa28319001
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 11:20:57 +0500

    Implement Find the Index of the First Occurrence in a String (LeetCode #28)

---
+++ LeetCodeSolutions/0028_FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,23 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Find the Index of the First Occurrence in a String - LeetCode #28
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) return 0;
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                string sub = haystack.Substring(i, needle.Length);
+                if (sub == needle)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
