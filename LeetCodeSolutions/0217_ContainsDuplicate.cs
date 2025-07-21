e8c426b93fe42cbbf93adc4dd3430fca59ec8a05
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 11:32:12 +0500

    Implement Contains Duplicate (LeetCode #217)

---
+++ LeetCodeSolutions/0217_ContainsDuplicate.cs
@@ -0,0 +1,23 @@
+﻿//- - - - - - - - - - - - - - - - - -
+// Contains Duplicate - LeetCode #217
+//- - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (set.Contains(num))
+                    return true;
+                set.Add(num);
+            }
+
+            return false;
+        }
+    }
+}
