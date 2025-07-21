7c6cc98844387c7665fbf3399f5f754e5e59570a
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 11:15:33 +0500

    Implement Remove Duplicates from Sorted Array (LeetCode #26)

---
+++ LeetCodeSolutions/0026_RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,27 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Remove Duplicates from Sorted Array - LeetCode #26
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int k = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[k - 1])
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+
+            return k;
+        }
+    }
+}
