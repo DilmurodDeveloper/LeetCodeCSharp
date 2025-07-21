65a1fb82a8d6726436c02b3e50d1ccdf4192ec33
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 11:24:23 +0500

    Implement Best Time to Buy and Sell Stock (LeetCode #121)

---
+++ LeetCodeSolutions/0121_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,25 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - -
+// Best Time to Buy and Sell Stock - LeetCode #121
+//- - - - - - - - - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            foreach (int price in prices)
+            {
+                if (price < minPrice)
+                    minPrice = price;
+                else if (price - minPrice > maxProfit)
+                    maxProfit = price - minPrice;
+            }
+
+            return maxProfit;
+        }
+    }
+}
