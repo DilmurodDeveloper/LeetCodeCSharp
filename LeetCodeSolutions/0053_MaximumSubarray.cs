2ef58f8293afc5c13d64c823c91515f7386aa1e6
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 11:49:54 +0500

    Implement Maximum Subarray (LeetCode #53)

---
+++ LeetCodeSolutions/0053_MaximumSubarray.cs
@@ -0,0 +1,23 @@
+﻿//- - - - - - - - - - - - - - - - -
+// Maximum Subarray - LeetCode #53
+//- - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = nums[0];
+            int currentSum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currentSum = Math.Max(nums[i], currentSum + nums[i]);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
