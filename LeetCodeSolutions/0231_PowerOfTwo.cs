e9cd6e0399d216b3f89d635bce986b854a1c8dcf
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 23:48:33 +0500

    Implement Power of Two (Leetcode #231)

---
+++ LeetCodeSolutions/0231_PowerOfTwo.cs
@@ -0,0 +1,16 @@
+﻿//- - - - - - - - - - - - - - - - - -
+// Power of Two - LeetCode #231
+//- - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0) return false;
+
+            return (n & (n - 1)) == 0;
+        }
+    }
+}
