b1b6d9860b33a3367151851e0f5bd9ae383499c6
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-24 00:13:00 +0500

    Implement Add Digits (LeetCode #258)

---
+++ LeetCodeSolutions/0258_AddDigits.cs
@@ -0,0 +1,25 @@
+﻿//- - - - - - - - - - - - - -
+// Add Digits - LeetCode #258
+//- - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public int AddDigits(int num)
+        {
+            while (num >= 10)
+            {
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+                num = sum;
+            }
+
+            return num;
+        }
+    }
+}
