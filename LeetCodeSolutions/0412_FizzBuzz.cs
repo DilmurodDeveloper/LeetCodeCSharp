2ce549fc1abba25333a815273112ea78164f6848
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-24 00:01:09 +0500

    Implement Fizz Buzz (LeetCode #412)

---
+++ LeetCodeSolutions/0412_FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿//- - - - - - - - - - - - - - 
+// Fizz Buzz - LeetCode #412
+//- - - - - - - - - - - - - - 
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> result = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+
+            return result;
+        }
+    }
+}
