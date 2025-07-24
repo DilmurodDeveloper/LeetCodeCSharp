785e72a9fd5064231259157d79939a47e312072d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-24 12:45:32 +0500

    Implement Length of Last Word (LeetCode #58)

---
+++ LeetCodeSolutions/0058_LengthOfLastWord.cs
@@ -0,0 +1,17 @@
+﻿//- - - - - - - - - - - - - - - - - -
+// Length of Last Word - LeetCode #58
+//- - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            List<string> words =
+                s.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            return words[words.Count - 1].Length;
+        }
+    }
+}
