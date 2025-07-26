12b747dcac53f6c3fdd9938f699753001ba6d30e
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-26 14:31:28 +0500

    Implement Ugly Number (LeetCode #263)

---
+++ LeetCodeSolutions/0263_UglyNumber.cs
@@ -0,0 +1,26 @@
+﻿//- - - - - - - - - - - - - - -
+// Ugly Number - LeetCode #263
+//- - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public bool IsUgly(int number)
+        {
+            if (number <= 0) return false;
+
+            int[] divisors = { 2, 3, 5 };
+
+            foreach (int divisor in divisors)
+            {
+                while (number % divisor == 0)
+                {
+                    number /= divisor;
+                }
+            }
+
+            return number == 1;
+        }
+    }
+}
