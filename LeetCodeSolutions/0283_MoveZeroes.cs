0a455313d8bdc5ba773173771492333d090ccc28
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-27 21:09:40 +0500

    Implement Move Zeroes (LeetCode #283)

---
+++ LeetCodeSolutions/0283_MoveZeroes.cs
@@ -0,0 +1,29 @@
+﻿//- - - - - - - - - - - - - - -
+// Move Zeroes - LeetCode #283
+//- - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int insertIndex = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[insertIndex] = nums[i];
+                    insertIndex++;
+                }
+            }
+
+            while (insertIndex < nums.Length)
+            {
+                nums[insertIndex] = 0;
+                insertIndex++;
+            }
+        }
+    }
+}
