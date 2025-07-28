fccfbd2998192121e0f41813ab3f930a384fa800
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-28 17:50:43 +0500

    Implement Detect Capital (LeetCode #520)

---
+++ LeetCodeSolutions/0520_DetectCapital.cs
@@ -0,0 +1,23 @@
+﻿//- - - - - - - - - - - - - - - -
+// Detect Capital - LeetCode #520
+//- - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            if (word == word.ToUpper())
+                return true;
+
+            if (word == word.ToLower())
+                return true;
+
+            if (char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower())
+                return true;
+
+            return false;
+        }
+    }
+}
