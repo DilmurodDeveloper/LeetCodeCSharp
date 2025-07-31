8bb0c34622229573aa4372c1c339d4a43a1a7e34
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-31 11:13:27 +0500

    Implement Path Sum (LeetCode #112)

---
+++ LeetCodeSolutions/0112_PathSum.cs
@@ -0,0 +1,37 @@
+﻿//- - - - - - - - - - - - -
+// Path Sum - LeetCode #112
+//- - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(
+            int val = 0,
+            TreeNode left = null,
+            TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public partial class Solution
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) return false;
+
+            if (root.left == null && root.right == null)
+                return root.val == targetSum;
+
+            int newTarget = targetSum - root.val;
+            return HasPathSum(root.left, newTarget) || HasPathSum(root.right, newTarget);
+        }
+    }
+}
