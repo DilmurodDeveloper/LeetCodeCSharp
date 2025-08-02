1f8f70518222cca0dccd9deed236b309815c278e
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-02 11:04:27 +0500

    Implement Jump Game (LeetCode #55)

---
+++ LeetCodeSolutions/0055_JumpGame.cs
@@ -0,0 +1,26 @@
+﻿//- - - - - - - - - - - - -
+// Jump Game - LeetCode #55
+//- - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            int farthest = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > farthest) return false;
+
+                farthest = Math.Max(farthest, i + nums[i]);
+
+                if (farthest >= nums.Length - 1)
+                    return true;
+            }
+
+            return true;
+        }
+    }
+}
