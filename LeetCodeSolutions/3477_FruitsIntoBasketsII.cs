169b64ca991ec2ed8dbacd8f87e64d68830f17d9
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 10:35:33 +0500

    Implement Fruits Into Baskets II (LeetCode #3477)

---
+++ LeetCodeSolutions/3477_FruitsIntoBasketsII.cs
@@ -0,0 +1,38 @@
+﻿//- - - - - - - - - - - - - - - - - - - - -
+// Fruits Into Baskets II - LeetCode #3477
+//- - - - - - - - - - - - - - - - - - - - -
+
+namespace LeetCodeSolutions
+{
+    public partial class Solution
+    {
+        public int NumOfUnplacedFruits(int[] fruits, int[] baskets)
+        {
+            int n = fruits.Length;
+            bool[] used = new bool[n];
+            int unplacedCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool placed = false;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (!used[j] && baskets[j] >= fruits[i])
+                    {
+                        used[j] = true;
+                        placed = true;
+                        break;
+                    }
+                }
+
+                if (!placed)
+                {
+                    unplacedCount++;
+                }
+            }
+
+            return unplacedCount;
+        }
+    }
+}
